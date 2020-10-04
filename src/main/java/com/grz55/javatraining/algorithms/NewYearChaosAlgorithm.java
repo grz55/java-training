5c8654633c730fbc9a3f11afdfc3ea6bd1ae9f10
Grz55 <grz55@vp.pl>
2020-10-04 19:26:36 +0200

    HackerRank New Year Chaos algorithm added

--- src/main/java/com/grz55/javatraining/algorithms/NewYearChaosAlgorithm.java
+++ src/main/java/com/grz55/javatraining/algorithms/NewYearChaosAlgorithm.java
@@ -7,10 +7,38 @@ public class NewYearChaosAlgorithm {
     // Complete the minimumBribes function below.
 
     // HACKER RANK
+    // New Year Chaos Algorithm
+    // For given input for example 1 2 5 3 7 8 6 4
+    // What's the minimum changes to happen to get 1 2 3 4 5 6 7 8 (sorted list)
+    // If any number from given input is more than 2 positions too far left - print Too chaotic
+    // For example 5 1 2 3 4 -> 5 is too far left - print Too chaotic
 
     static void minimumBribes(int[] q) {
+        for (int i = 0; i < q.length; i++) {
+            if (q[i] > i + 3) {
+                System.out.println("Too chaotic");
+                return;
+            }
+        }
 
-        System.out.println("Too chaotic");
+        int temp = 0;
+        int bribes = 0;
+        boolean bribeHappened = true;
+
+        while (bribeHappened) {
+            bribeHappened = false;
+
+            for (int i = 0; i < q.length - 1; i++) {
+                if (q[i] > q[i + 1]) {
+                    temp = q[i + 1];
+                    q[i + 1] = q[i];
+                    q[i] = temp;
+                    bribes++;
+                    bribeHappened = true;
+                }
+            }
+        }
+        System.out.println(bribes);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
