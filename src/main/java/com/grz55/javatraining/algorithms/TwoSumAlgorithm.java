2eda8b0f223db2c9258b7628af62aa6ccd1b2588
Grz55 <grz55@vp.pl>
2020-10-29 23:43:10 +0100

    Variable names refactored

--- src/main/java/com/grz55/javatraining/algorithms/TwoSumAlgorithm.java
+++ src/main/java/com/grz55/javatraining/algorithms/TwoSumAlgorithm.java
@@ -6,25 +6,25 @@ public class TwoSumAlgorithm {
     // LEET CODE
     // Find which numbers added together give results provided in 'target'
 
-    public static int[] twoSum(int[] nums, int target) {
-        int[] result = {0, 0};
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    result[0] = nums[i];
-                    result[1] = nums[j];
-                    return result;
+    public static int[] twoSum(int[] inputNumbers, int target) {
+        int[] resultNumbers = {0, 0};
+        for (int i = 0; i < inputNumbers.length; i++) {
+            for (int j = i + 1; j < inputNumbers.length; j++) {
+                if (inputNumbers[i] + inputNumbers[j] == target) {
+                    resultNumbers[0] = inputNumbers[i];
+                    resultNumbers[1] = inputNumbers[j];
+                    return resultNumbers;
                 }
             }
         }
-        return result;
+        return resultNumbers;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 18;
-        int[] result = twoSum(nums, target);
-        System.out.println("Result " + result[0] + "+" + result[1] + "=" + target);
+        int[] inputNumbers = {2, 7, 11, 15};
+        int targetSum = 18;
+        int[] resultNumbers = twoSum(inputNumbers, targetSum);
+        System.out.println("Result " + resultNumbers[0] + "+" + resultNumbers[1] + "=" + targetSum);
     }
 
 }
